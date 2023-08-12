b7dca088e4a1173f3bb6414c9c896213dba7b4ab
kupns <kupnsaloxa@gmail.com>
2023-08-12 14:56:29 +0300

    format

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,30 @@ extern crate chrono;
 
 use diesel::prelude::*;
 
-pub mod schema;
 pub mod models;
+pub mod schema;
 
-use std::default::Default;
-use diesel::{
-    Connection, PgConnection,
-    pg::Pg,
-    debug_query,
-};
-use dotenvy::dotenv;
-use std::env;
-use std::net::SocketAddr;
-use serde::{Deserialize, Serialize};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use axum::{
     extract::State,
-    routing::{get, post},
     http::StatusCode,
     response::IntoResponse,
+    routing::{get, post},
     Json, Router,
 };
+use diesel::{debug_query, pg::Pg, Connection, PgConnection};
+use dotenvy::dotenv;
+use serde::{Deserialize, Serialize};
+use std::default::Default;
+use std::env;
+use std::net::SocketAddr;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
 use axum::handler::Handler;
-use tokio_postgres::NoTls;
 use postdamn::{
-    models::{User, Role},
-    services::{Example},
+    models::{Role, User},
+    services::Example,
 };
+use tokio_postgres::NoTls;
 
 pub fn establish_connection() -> PgConnection {
     let url = env::var("DATABASE_URL").expect("Database url env var not set");
@@ -46,7 +43,9 @@ async fn get_users_list() -> (StatusCode, Json<Vec<User>>) {
         .then_order_by(id)
         .limit(5);
     tracing::debug!("Run SQL {}", debug_query::<Pg, _>(&q));
-    let u: Vec<User> = q.load::<User>(& mut connection).expect("Error loading users");
+    let u: Vec<User> = q
+        .load::<User>(&mut connection)
+        .expect("Error loading users");
     (StatusCode::OK, Json(u))
 }
 
@@ -79,12 +78,9 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let user_routes = Router::new()
-        .route("/", get(get_users_list).post(post_user));
-    let api_routes = Router::new()
-        .nest("/users", user_routes);
-    let app = Router::new()
-        .nest("/api/:version", api_routes);
+    let user_routes = Router::new().route("/", get(get_users_list).post(post_user));
+    let api_routes = Router::new().nest("/users", user_routes);
+    let app = Router::new().nest("/api/:version", api_routes);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
