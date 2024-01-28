ea2414a355e823b686a3344d26457d5f1e42068e
kupns <kupnsaloxa@gmail.com>
2024-01-28 19:15:34 +0300

    add python cat detector

---
+++ catify/dataset.py
@@ -0,0 +1,63 @@
+import os
+import torch
+
+from torchvision.io import read_image
+from torchvision.ops.boxes import masks_to_boxes
+from torchvision import datapoints as dp
+from torchvision.transforms.v2 import functional as F
+
+
+class PennFudanDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+        img = read_image(img_path)
+        mask = read_image(mask_path)
+        # instances are encoded as different colors
+        obj_ids = torch.unique(mask)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+        num_objs = len(obj_ids)
+
+        # split the color-encoded mask into a set
+        # of binary masks
+        masks = (mask == obj_ids[:, None, None]).to(dtype=torch.uint8)
+
+        # get bounding box coordinates for each mask
+        boxes = masks_to_boxes(masks)
+
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        # Wrap sample and targets into torchvision datapoints:
+        img = dp.Image(img)
+
+        target = {}
+        target["boxes"] = dp.BoundingBoxes(boxes, format="XYXY", canvas_size=F.get_size(img))
+        target["masks"] = dp.Mask(masks)
+        target["labels"] = labels
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
--- catify/main.py
+++ catify/main.py
@@ -1,6 +1,26 @@
-import torch
+import cv2
+import matplotlib.pyplot as plt
+import pandas as pd
 
-torch.load('models/resnet50-0676ba61.pth')
+
+def load_image(file_name):
+    cv_img = cv2.imread(file_name)
+    #     return cv2.resize(cv_img, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
+    return cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+
+
+def load_label(file_name):
+    df = pd.read_csv(file_name + ".cat", sep=" ", header=None)
+    df = df.drop(columns=[0, 19])
+    return df.values.squeeze()
+
 
 if __name__ == '__main__':
-    pass
+    file = 'datasets/CAT_00/00000001_000.jpg'
+    image_in_numpy = load_image(file)
+    label_in_numpy = load_label(file)
+
+    plt.scatter(label_in_numpy[::2], label_in_numpy[1::2], color='yellow')
+    plt.imshow(image_in_numpy)
+    plt.show()
+    print(label_in_numpy)
